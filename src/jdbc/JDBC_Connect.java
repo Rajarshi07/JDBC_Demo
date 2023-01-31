7d86d33e39ab164ac76ab06778950695305bcc0e
rajarshid <rajarshid@trainee.nrifintech.com>
2023-01-31 17:02:40 +0530

    first commit

---
+++ src/jdbc/JDBC_Connect.java
@@ -0,0 +1,31 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBC_Connect {
+
+	public JDBC_Connect() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/orcl","rajarshi","Oracle_1");
+			Statement smt=con.createStatement();
+			smt.executeUpdate("CREATE TABLE EMP(ENO NUMBER,ENAME VARCHAR(20),ESAL NUMBER)");
+//			boolean out = smt.execute("SELECT * FROM EMPLOYEE");
+//			smt.getResultSet();
+			
+			System.out.println("Connection");
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
